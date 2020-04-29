44cc39fd45c37b2a3a1a3787001f1988f7d3d3a4
aejb <thomas.bass2001@gmail.com>
2020-04-29 15:34:16 +0000

    adds wk3 notes

---
+++ wk3/notes.md
@@ -0,0 +1,29 @@
+# Week 3 Notes
+
+## Big O
+
+Order of the upper bound (on the order of worst case)
+
+From worst...
+
+* O(n^2) - bubble, selection
+* O(n log n) - merge sort
+* O(n) - linear
+* O(log n) - binary
+* O(1) -
+*
+... to best
+
+## Omega
+
+Best case!
+
+* Om(n^2) - selection
+* Om(n log n)
+* Om(n) - bubble
+* Om(log n)
+* Om(1) - linear search, binary search
+
+## Theta
+
+Upper and lower are the same
---
+++ wk3/sc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+typedef struct
+{
+    string name;
+    string number;
+}
+person;
+
+int main(void)
+{
+    int numbers[6] = {4, 8, 15, 16, 23, 42}; // static initialized
+    string names[1] = {"NAME"};
+    if (strcmp(names[0], "NAME") == 0) // strcmp
+    {
+        // printf("Not here\n");
+    }
+    
+    //structs
+    
+    person people[1];
+    people[0].name = "EMMA";
+    people[0].number = "617-555-0101";
+    printf("%s\n", people[0].name);
+}
