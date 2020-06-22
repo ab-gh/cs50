2fd261d2dbe109d80a63785476b02cb4c734c795
aejb <thomas.bass2001@gmail.com>
2020-06-22 21:02:08 +0000

    shit i need an idea for my 4th page

--- wk8/homepage/main.js
+++ wk8/homepage/main.js
@@ -1,3 +1,37 @@
 $(document).ready(function(){
   $('.header').height($(window).height());
 })
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    confidentBox = document.querySelector(".confident");
+    learningBox = document.querySelector(".learning");
+    plannedBox = document.querySelector(".planned");
+    confidentArrow = document.querySelector(".confident-arrow");
+    learningArrow = document.querySelector(".learning-arrow");
+    plannedArrow = document.querySelector(".planned-arrow");
+    document.querySelector("#confident").onclick = function() {
+        confidentBox.style.display = confidentArrow.style.display = "inline";
+        learningBox.style.display = learningArrow.style.display = "none";
+        plannedBox.style.display = plannedArrow.style.display = "none";
+    };
+    document.querySelector("#learning").onclick = function() {
+        confidentBox.style.display = confidentArrow.style.display = "none";
+        learningBox.style.display = learningArrow.style.display = "inline";
+        plannedBox.style.display = plannedArrow.style.display = "none";
+    };
+    document.querySelector("#planned").onclick = function() {
+        confidentBox.style.display = confidentArrow.style.display = "none";
+        learningBox.style.display = learningArrow.style.display = "none";
+        plannedBox.style.display = plannedArrow.style.display = "inline";
+    }
+});
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector("#reload").onclick = function() {
+        location.reload();
+        return false;
+    }
+});
